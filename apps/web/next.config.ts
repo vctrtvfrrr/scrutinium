ec585595fd2873a3f57dd91d4462e8d852ff9fe3
Victor Otávio Ferreira <victor@ferreira.id>
2025-11-18 12:24:49 -0300

    fix: configure better-sqlite3 for next

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ["better-sqlite3"],
+  },
+  webpack: (config) => {
+    config.externals = config.externals ?? [];
+    if (!config.externals.includes("better-sqlite3")) {
+      config.externals.push("better-sqlite3");
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
